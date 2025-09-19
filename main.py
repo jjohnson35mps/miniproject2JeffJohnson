0635e38ae566440ec56f15726e3641acb193b8ef
Jeff Johnson <j_johnson35@mail.fhsu.edu>
2025-09-18 20:20:54 -0400

    should be final commit... I was wrong... last change

--- main.py
+++ main.py
@@ -16,7 +16,7 @@
 # ✓(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 # ✓(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # ✓(10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
-#(20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations.
+# ✓(20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations.
 # pip install pandas
 # pip install kagglehub
 # pip install matplotlib
